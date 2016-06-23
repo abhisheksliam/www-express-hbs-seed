153843833f9986ee1d696a3a2ae00d7b7e8ac44a
shipra <shipra.bathla@comprotechnologies.com>
2016-06-23 18:46:52 +0530

    Child node error was coming from template: As now we have nested view so content was not loaded and element was not available. Moved code to Scriptor as of now.

--- app/modules/core/core.controller.js
+++ app/modules/core/core.controller.js
@@ -15,36 +15,9 @@
 			Dropzone.autoDiscover = false;
 		});
 
-		$scope.$on('$viewContentLoaded', function () {
-			pluginsService.init();
-			// ChildNode Undefined error is coming from this, so commented
-			//	applicationService.customScroll();
-			applicationService.handlePanelAction();
-			$('.nav.nav-sidebar .nav-active').removeClass('nav-active active');
-			$('.nav.nav-sidebar .active:not(.nav-parent)').closest('.nav-parent').addClass('nav-active active');
-
-			if($location.$$path == '/' || $location.$$path == '/layout-api'){
-				$('.nav.nav-sidebar .nav-parent').removeClass('nav-active active');
-				$('.nav.nav-sidebar .nav-parent .children').removeClass('nav-active active');
-				if ($('body').hasClass('sidebar-collapsed') && !$('body').hasClass('sidebar-hover')) return;
-				if ($('body').hasClass('submenu-hover')) return;
-				$('.nav.nav-sidebar .nav-parent .children').slideUp(200);
-				$('.nav-sidebar .arrow').removeClass('active');
-			}
-			if($location.$$path == '/'){
-				$('body').addClass('dashboard');
-			}
-			else{
-				$('body').removeClass('dashboard');
-			}
-
-		});
-
 		$scope.isActive = function (viewLocation) {
 			return viewLocation === $location.path();
 		};
-
-	  
 	  
   }
 
--- app/modules/scriptor/scriptor.controller.js
+++ app/modules/scriptor/scriptor.controller.js
@@ -4,8 +4,30 @@
   angular.module('automationApp.scriptor')
     .controller('NewScriptController', NewScriptController);
 
-  NewScriptController.$inject = ['$scope'];
-  function NewScriptController($scope) {
+  NewScriptController.$inject = ['$scope', 'pluginsService', 'applicationService', '$location'];
+  function NewScriptController($scope, pluginsService, applicationService, $location) {
+	  
+	  /* Template Code to be kept in first route to be loaded */
+	  $scope.$on('$viewContentLoaded', function () {
+			pluginsService.init();
+			applicationService.customScroll();
+			applicationService.handlePanelAction();
+			$('.nav.nav-sidebar .nav-active').removeClass('nav-active active');
+			$('.nav.nav-sidebar .active:not(.nav-parent)').closest('.nav-parent').addClass('nav-active active');
+
+			if($location.$$path == '/'){
+				$('.nav.nav-sidebar .nav-parent').removeClass('nav-active active');
+				$('.nav.nav-sidebar .nav-parent .children').removeClass('nav-active active');
+				if ($('body').hasClass('sidebar-collapsed') && !$('body').hasClass('sidebar-hover')) return;
+				if ($('body').hasClass('submenu-hover')) return;
+				$('.nav.nav-sidebar .nav-parent .children').slideUp(200);
+				$('.nav-sidebar .arrow').removeClass('active');
+				$('body').addClass('dashboard');
+			} else {
+				$('body').removeClass('dashboard');
+			}
+
+	  });
   
   }
 
