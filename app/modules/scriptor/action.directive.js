6fc07ec4e90531dd9aa6d765a6edaa8d7a2f2cf1
shipra <shipra.bathla@comprotechnologies.com>
2016-07-08 14:23:36 +0530

    Item Panel and Trigger UI updates

--- app/modules/scriptor/action.directive.js
+++ app/modules/scriptor/action.directive.js
@@ -15,6 +15,26 @@ angular.module('automationApp.scriptor')
             },
             link: function (scope, element, attributes) {
 
+                // Toggle Panel Content
+                element.find(".panel-header .panel-toggle").click(function (event) {
+                    event.preventDefault();
+                    $(this).toggleClass("closed").parents(".panel:first").find(".panel-content").slideToggle();
+                });
+
+                element.find(".panel-content").slideToggle();
+
+                element.find(".panel-header .panel-close").click(function (event) {
+                    event.preventDefault();
+                    var $item = $(this).parents(".dd-item:first");
+                    bootbox.confirm("Are you sure to remove this panel?", function (result) {
+                        if (result === true) {
+                            $item.addClass("animated bounceOutRight");
+                            window.setTimeout(function () {
+                                $item.remove();
+                            }, 300);
+                        }
+                    });
+                });
             }
         }
     });
