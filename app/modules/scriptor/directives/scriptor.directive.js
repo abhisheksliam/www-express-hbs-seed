7e10801271b2f3548f2a1a2e577047f96db8bf96
shipra <shipra.bathla@comprotechnologies.com>
2016-07-13 12:19:24 +0530

    Baloo Content auto close on item switch and Add Method Button

--- app/modules/scriptor/directives/scriptor.directive.js
+++ app/modules/scriptor/directives/scriptor.directive.js
@@ -24,7 +24,7 @@ angular.module('automationApp.scriptor')
                 });
 
                 element.find( ".li-level-0" ).sortable({
-                    items: "ol",
+                    items: "ol:not(.ui-sort-disabled)",
                     placeholder: "placeholder-ui",
                     handle: ".item-level-1"
                 });
@@ -62,11 +62,19 @@ angular.module('automationApp.scriptor')
                     var level1items = element.find('.selected');
                     level1items.siblings(".data-items").hide();
                     level1items.removeClass('selected')
+
+
                 }
 
                 var  closeLevel2Elements = function () {
                     var level2items = element.find('.item-level-2 .panel-toggle.closed');
                     level2items.toggleClass("closed").parents(".panel:first").find(".panel-content").slideToggle();
+                    var content = element.find(".baloo-action-content:visible");
+                    if(content.length !=0) {
+                        content.siblings(".data-items").width("100%");
+                        content.hide();
+                    }
+
                 }
 
                 element.find(".item-level-1.dd3-content").click(function (event) {
@@ -102,7 +110,7 @@ angular.module('automationApp.scriptor')
                 element.find(".li-level-1 .baloo-action-close").click(function (event) {
                     event.preventDefault();
 
-                    var content = $(this).parent().parent();
+                    var content = $(this).closest(".baloo-action-content");
                     content.siblings(".data-items").width("100%");
                     content.hide();
 
