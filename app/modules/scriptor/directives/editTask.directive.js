6f4e73c626de438bf78d91ea72f6cbd4770f9663
Abhishek <abhishek.kumar@comprotechnologies.com>
2016-08-12 15:51:56 +0530

    feature update for template choice feedback updates

--- app/modules/scriptor/directives/editTask.directive.js
+++ app/modules/scriptor/directives/editTask.directive.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('automationApp.scriptor')
-    .directive('editTask', ['$timeout','pluginsService', function($timeout,pluginsService) {
+    .directive('editTask', ['$timeout', function($timeout) {
         return {
             restrict: 'E',
             replace: true,
@@ -13,25 +13,6 @@ angular.module('automationApp.scriptor')
                         dropdownCssClass: 'form-white',
                         minimumResultsForSearch: -1
                     });
-
-/*                    $(':radio').each(function () {
-
-                        var checkboxClass = $(this).attr('data-checkbox') ? $(this).attr('data-checkbox') : 'icheckbox_minimal-grey';
-                        var radioClass = $(this).attr('data-radio') ? $(this).attr('data-radio') : 'iradio_minimal-grey';
-
-                        if (checkboxClass.indexOf('_line') > -1 || radioClass.indexOf('_line') > -1) {
-                            $(this).iCheck({
-                                checkboxClass: checkboxClass,
-                                radioClass: radioClass,
-                                insert: '<div class="icheck_line-icon"></div>' + $(this).attr("data-label")
-                            });
-                        } else {
-                            $(this).iCheck({
-                                checkboxClass: checkboxClass,
-                                radioClass: radioClass
-                            });
-                        }
-                    });*/
 
                 },200);
 
