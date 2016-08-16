abfdbdcfdba7b2d4b644458ff1904da67e6a2407
shipra <shipra.bathla@comprotechnologies.com>
2016-08-16 17:15:57 +0530

    Added Created_by and last_modified_by properties in schema.

--- server/models/app.server.models.script.js
+++ server/models/app.server.models.script.js
@@ -3,30 +3,32 @@ var Schema       = mongoose.Schema;
 
 var automationScriptsSchema   = new Schema({
     sle_id: String,
-   /* created_by: {
+    created_by: {
         _id: {
-            type: mongoose.Schema.ObjectId,
+            type: mongoose.Schema.ObjectId    /*,
             required: 'createdBy cannot be blank',
-            trim: true
+            trim: true*/
         },
         name: {
             type: String,
+            default: 'Compro',
             trim: true,
             required: 'createdBy cannot be blank'
         }
     },
     modified_by: {
         _id: {
-            type: mongoose.Schema.ObjectId,
+            type: mongoose.Schema.ObjectId/*,
             required: 'createdBy cannot be blank',
-            trim: true
+            trim: true*/
         },
         name: {
             type: String,
+            default: 'Compro',
             trim: true,
             required: 'createdBy cannot be blank'
         }
-    },*/
+    },
     created_on: {
         type: Date,
         default: Date.now
