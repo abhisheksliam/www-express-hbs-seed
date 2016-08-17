f4ff184d8cab7774be7199e4c3d104aba77f4f27
shipra <shipra.bathla@comprotechnologies.com>
2016-08-17 19:22:18 +0530

    Added directive for Runner Launcher rendering

---
+++ app/modules/runner/runner.directive.js
@@ -0,0 +1,14 @@
+"use strict";
+
+angular.module('automationApp')
+    .directive('runnerLauncher', function() {
+        return {
+            restrict: 'A',
+            templateUrl: 'modules/runner/runnerLauncher.tpl.html',
+            link: function (scope, element, attributes) {
+
+
+            }
+        }
+    });
+
