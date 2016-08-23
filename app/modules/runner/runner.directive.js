d87d89bbd77d317e94d79caa602176cf125fce0d
Piyush Mehta <piyush.mehta@comprotechnologies.com>
2016-08-23 15:43:43 +0530

    runner launcher ux round1
    Added all required buttons and select - with minor issues.

--- app/modules/runner/runner.directive.js
+++ app/modules/runner/runner.directive.js
@@ -28,14 +28,30 @@ angular.module('automationApp.runner')
 
                     // checkbox button update
                     element.on('click',".item-level-0.dd3-content .item-check",function(event) {
-                        $(".runtaskbtn").addClass("disabled");
+                        if(
+                            $(".item-level-0.dd3-content .item-check").each(function(index) {
+                                if($(this).attr("checked")) {
+                                    return false;
+                                }
+                            })
+                        ) {
+                            $(".runtaskbtn").prop('disabled', false);
+                            $(".runtaskbtn").removeClass("disablebtn").addClass("enablebtn");
+                        }
+                        else {
+                            $(".runtaskbtn").prop('disabled', true);
+                            $(".runtaskbtn").removeClass("enablebtn").addClass("disablebtn");
+                        }
                         event.stopPropagation();
                     });
 
                     // run task button
                     element.on('click',".runtaskbtn",function(event) {
-                        console.log('clicking run task button');
+                        event.stopPropagation();
+                    });
 
+                    // run task button
+                    element.on('hover',".runtaskbtn",function(event) {
                         event.stopPropagation();
                     });
                 });
