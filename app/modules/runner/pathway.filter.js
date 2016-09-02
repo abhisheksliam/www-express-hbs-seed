8ee9e3ecc1683ff2e10d17a6862f6272303cfd11
shipra <shipra.bathla@comprotechnologies.com>
2016-09-02 19:31:10 +0530

    Runner Filter added

--- app/modules/runner/pathway.filter.js
+++ app/modules/runner/pathway.filter.js
@@ -3,14 +3,16 @@
 angular.module('automationApp.runner')
     .filter('pathwayFilter', function() {
         return function(items){
-            var newArray = [];
-            for(var x = 0; x < items.length; x+=2){
-                newArray.push({
-                    "pathway" : items[x],
-                    "group": items[x + 1]
-                });
-            }
+            if(items !== undefined) {
+                var newArray = [];
+                for(var x = 0; x < items.length; x+=2){
+                    newArray.push({
+                        "pathway" : items[x],
+                        "group": items[x + 1]
+                    });
+                }
 
-            return newArray;
+                return newArray;
+            }
         }
     });
