b64c335dd013004faae008577a6d4ae522553432
unknown <shipra@Shipra.COMPRO>
2016-09-09 18:49:46 +0530

    Load Modal Updates

--- app/modules/sidebar/loadModal.directive.js
+++ app/modules/sidebar/loadModal.directive.js
@@ -1,60 +1,64 @@
 "use strict";
 
 angular.module('automationApp.sidebar')
-    .directive('loadModal', ['scriptorService', function(scriptorService) {
+    .directive('loadModal', ['scriptorService','$rootScope', function(scriptorService, $rootScope) {
         return {
             restrict: 'E',
             replace: true,
             scope:{},
             templateUrl: 'modules/sidebar/loadModal.tpl.html',
             link: function (scope, element, attributes) {
-                scope.task;
+                scope.loadTaskOption = "";
+                scope.loadTaskId ="";
 
-                scope.loadScript = function(){
-                    var ingestJSON;
-                    var file = document.getElementById('files').files[0];
-                    if (file) {
-                        // create reader
-                        var reader = new FileReader();
-                        reader.readAsText(file);
-                        reader.onload = function(e) {
-                            ingestJSON = JSON.parse(e.target.result)[0];
-
-                            // TO-DO: Event trigger ..... go to save
-                            scriptorService.saveTaskScript(ingestJSON.appName, ingestJSON.scenario, ingestJSON.id, '', 'ingest', ingestJSON, username).then(function(res) {
-
-
-                                scriptorService.taskContent = res.data.task_json;
-                                $('#modal-loadtask').modal('hide');
-                               // $state.go('app.script-editor',  {id: res.data.sle_id});
-                              //  $scope.showNotify('<div class="alert alert-success m-r-30"><p><strong>' + 'Task data updated successfully !' + '</p></div>');
-
-                            });
-                        };
-                    }
-
-
-                    /* if ($scope.loadTaskId == undefined || $scope.loadTaskId.length === 0) {
-                     $scope.showNotify('<div class="alert alert-danger"><p><strong>' + 'Task Id cannot be blank !' + '</p></div>','.modal-body');
-                     }
-                     else if ($scope.validateTaskId($scope.loadTaskId)){	// client side validation
-                     // api call
-                     scriptorService.getTaskJson($scope.loadTaskId).then(function(res) {
-                     if (res.data.length == 0) {
-                     $scope.showNotify('<div class="alert alert-danger"><p><strong>Error in getting Task Data</p></div>','.modal-body');
-                     } else{
-                     $('#modal-loadtask').modal('hide');
-                     scriptorService.taskContent = res.data[0].task_json;
-                     $state.go('app.script-editor',  {id: res.data[0].sle_id});
-                     $scope.showNotify('<div class="alert alert-success m-r-30"><p><strong>' + 'Task data loaded successfully !' + '</p></div>');
-                     }
-                     });
-                     //
-                     } else{
-                     $scope.showNotify('<div class="alert alert-danger"><p><strong>' + 'Invalid Task Id !' + '</p></div>','.modal-body');
-                     }*/
+                scope.iCheckOptions = {
+                    radioClass: 'iradio_flat-blue'
                 };
 
+                scope.loadScript = function(){
+                    if ($('input[name=load-task-options]:checked').val() === "2") {
+                        var ingestJSON;
+                        var file = document.getElementById('files').files[0];
+                        if (file) {
+                            // create reader
+                            var reader = new FileReader();
+                            reader.readAsText(file);
+                            reader.onload = function(e) {
+                                ingestJSON = JSON.parse(e.target.result)[0];
+
+                                scriptorService.saveTaskScript(ingestJSON.appName, ingestJSON.scenario, ingestJSON.id, '', 'ingest', ingestJSON, username).then(function(res) {
+                                    if(res.data.errors) {
+                                        if(res.data.errors.errorCode === 'EXISTS_IN_DB'){
+                                            $rootScope.showNotify('<div class="alert alert-danger m-r-30"><p><strong>Task already exists</p></div>','.modal-body');
+                                        } else {
+                                            $rootScope.showNotify('<div class="alert alert-danger m-r-30"><p><strong>' + res.data.errors.errorMessage + '</p></div>','.modal-body');
+                                        }
+                                    } else{
+                                        $('#modal-loadtask').modal('hide');
+                                        $rootScope.$broadcast('SCRIPTOR_LOAD_TASK', res.data);
+                                    }
+                                });
+                            };
+                        }
+                    } else {
+                        if (scope.loadTaskId == undefined || scope.loadTaskId.length === 0) {
+                            $rootScope.showNotify('<div class="alert alert-danger"><p><strong>' + 'Task Id cannot be blank !' + '</p></div>','.modal-body');
+                        }
+                        else if ($rootScope.validateTaskId(scope.loadTaskId)){	// client side validation
+                            // api call
+                            scriptorService.getTaskJson(scope.loadTaskId).then(function(res) {
+                                if (res.data.length == 0) {
+                                    $rootScope.showNotify('<div class="alert alert-danger"><p><strong>Error in getting Task Data</p></div>','.modal-body');
+                                } else{
+                                    $('#modal-loadtask').modal('hide');
+                                    $rootScope.$broadcast('SCRIPTOR_LOAD_TASK', res.data[0]);
+                                }
+                            });
+                        } else{
+                            $rootScope.showNotify('<div class="alert alert-danger"><p><strong>' + 'Invalid Task Id !' + '</p></div>','.modal-body');
+                        }
+                    }
+                };
             }
         }
 }]);
