dd7f70d5419df0f46ebf2628e9185decd3006c9f
unknown <shipra@Shipra.COMPRO>
2016-09-29 16:47:20 +0530

    Issue due to usage of data attribute. data should be used for static content.

--- app/modules/scriptor/directives/trigger.directive.js
+++ app/modules/scriptor/directives/trigger.directive.js
@@ -51,7 +51,7 @@ angular.module('automationApp.scriptor')
                     event.preventDefault();
 
                     if(scope.triggerForm.$valid) {
-                        var triggerNumber = parseInt($(this).closest('.li-level-2').data('id'));
+                        var triggerNumber = parseInt($(this).closest('.dd-list').index());
 
                         var triggerRefrence = $(this).closest('.dd-list');
 
@@ -165,7 +165,7 @@ angular.module('automationApp.scriptor')
                         });
                         scope.$apply();
                     } else {
-                        var triggerNumber = parseInt($(this).closest('.li-level-2').data('id'));
+                        var triggerNumber = parseInt($(this).closest('.dd-list').index());
 
                         bootbox.confirm("Are you sure to remove this trigger?", function (result) {
                             if (result === true) {
@@ -190,7 +190,7 @@ angular.module('automationApp.scriptor')
                 element.on('click',".copy-trigger",function (event) {
                     event.preventDefault();
 
-                    var triggerNumber = parseInt($(this).closest('.li-level-2').data('id'));
+                    var triggerNumber = parseInt($(this).closest('.dd-list').index());
                     var triggerToCopy = angular.copy(scope.method.actions[triggerNumber]);
 
                     scope.method.actions.splice(triggerNumber, 0, triggerToCopy);
